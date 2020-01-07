7aefad6ae7b313274bcc72d86b726a04e8a774ba
double beep <38133098+double-beep@users.noreply.github.com>
2020-01-07 20:40:34 +0200

    Formatting, no redefinition of variables when main function is called
    Add semicolons, simplifications (don't declare a variable and use it once), use const.

--- tools/compare-answers/compare-answers.js
+++ tools/compare-answers/compare-answers.js
@@ -1,19 +1,19 @@
-$("#error").hide()
-$("#data").hide()
-var site_name = null
+$("#error").hide();
+$("#data").hide();
+var site_name = null;
+const question_pattern = /https:\/\/((((.+)\.)stackexchange|stackoverflow|superuser|serverfault|askubuntu|stackapps)\.com|mathoverflow\.net)\/(q|questions)\/(\d+)(\/.*)?$/i;
+const api_url = STACKEXCHANGE_API + "/questions";
 
-function fetch_answers(){
-    var api_url = STACKEXCHANGE_API+"/questions"
-    var post_url = $("#postLink").val()
-    var question_pattern = /https:\/\/((((.+)\.)stackexchange|stackoverflow|superuser|serverfault|askubuntu|stackapps)\.com|mathoverflow\.net)\/(q|questions)\/(\d+)(\/.*)?$/i
-    var matched_question = post_url.match(question_pattern)
+function fetch_answers() {
+    var post_url = $("#postLink").val();
+    var matched_question = post_url.match(question_pattern);
     if (matched_question){
-        console.log("matched question")
-        site_name = matched_question[1]
-        var post_id = matched_question[6]
+        console.log("matched question");
+        site_name = matched_question[1];
+        var post_id = matched_question[6];
     }
     else {
-        error("Can't find match, check URL.")
+        display_error("Can't find match, check URL.");
         return;
     }
 
@@ -23,64 +23,55 @@ function fetch_answers(){
         "sort": "creation",
         "filter": "!b1MMEbc8q)iw*H"
     }
-    $.get(api_url+"/"+post_id+"/answers", data, success)
+    $.get(api_url + "/" + post_id + "/answers", data, success);
 
 }
 
 function success(return_data) {
-    console.log(return_data)
-    quota = return_data["quota_remaining"]
-    $("#quota").html(quota)
-    $("#data-table").html("<thead><tr><th>Created On</th><th>Score</th><th>Up Vote Count</th><th>Down Vote Count</th><th>User</th></tr></thead>")
+    console.log(return_data);
+    $("#quota").html(return_data.quota_remaining);
+    $("#data-table").html("<thead><tr><th>Created On</th><th>Score</th><th>Up Vote Count</th><th>Down Vote Count</th><th>User</th></tr></thead>");
 
-    if(return_data["items"] == false) {
+    if(return_data.items == false) {
         display_error("Could not load post, might be deleted");
         return;
     }
-    var answers = return_data["items"]
+    var answers = return_data.items;
 
-    if(answers.length == 0) {
+    if (answers.length == 0) {
         display_error("There are no answers to this question");
         return;
     }
 
-    var tbody = $("<tbody></tbody>")
-    for (i=0; i<answers.length; i++){
+    var tbody = $("<tbody></tbody>");
+    for (i = 0; i < answers.length; i++) {
 
-        var answer = answers[i]
-        var is_accepted = answer["is_accepted"]
+        var answer = answers[i];
 
-        var creation_date = new Date(answer["creation_date"]*1000).toISOString()
-        var link = answer["link"]
-        var creation_date_col = "<td><a href='"+link+"'>"+creation_date+"<a></td>"
+        var creation_date = new Date(answer.creation_date * 1000).toISOString();
+        var creation_date_col = "<td><a href='" + answer.link + "'>" + creation_date + "<a></td>";
 
-        var upv_count = answer["up_vote_count"]
-        var upv_count_col = "<td>"+upv_count+"</td>"
+        var upvote_count_col = "<td>" + answer.up_vote_count + "</td>";
+        var downvote_count_col = "<td>" + answer.down_vote_count + "</td>";
+        var score_col = "<td>" + answer.score + "</td>";
 
-        var dnv_count = answer["down_vote_count"]
-        var dnv_count_col = "<td>"+dnv_count+"</td>"
+        var userid = answer.owner.user_id;
+        var flair = "<td><a href='" + answer.owner.link + "'><img alt='" + userid + "' src='https://" + site_name + "/users/flair/" + userid + ".png' /></a></td>";
 
-        var score = answer["score"]
-        var score_col = "<td>"+score+"</td>"
-
-        var userid = answer["owner"]["user_id"]
-        var userlink = answer["owner"]["link"]
-        var flair = "<td><a href='"+userlink+"'><img alt='"+userid+"' src='https://"+site_name+"/users/flair/"+userid+".png' /></a></td>"
-
-        var row_type = "<tr>"
-        if (is_accepted) {
-            row_type = "<tr class='accepted'>"
+        var row_type = "<tr>";
+        if (answer.is_accepted) {
+            row_type = "<tr class='accepted'>";
         }
-        var div_string = row_type + creation_date_col + score_col + upv_count_col + dnv_count_col + flair + '</tr>'
-        var row = $(div_string)
+        var div_string = row_type + creation_date_col + score_col + upvote_count_col + downvote_count_col + flair + '</tr>';
+        var row = $(div_string);
 
-        tbody.append(row)
-
+        tbody.append(row);
     }
-    $("#data-table").append(tbody)
+
+    $("#data-table").append(tbody);
     $("#data-table").DataTable( {"paging":   false,
                                  "searching": false,
                                  "info":     false,
-                                 "destroy": true,})
-    $("#data").show()
+                                 "destroy": true,});
+    $("#data").show();
 }
