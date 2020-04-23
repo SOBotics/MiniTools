SOBotics/MiniTools

commit 6c2a1b22a77a40d2d89fa6b1b412fea19d83ae14

Format CSS file for readability

styles/custom.css
@@ -1,5 +1,19 @@
-.nav-span{color:white;}
-.main-content{padding-top:20px;}
-.page-header{padding-bottom:20px;}
-.pad-top{padding-top:20px;}
-.accepted{background-color:#bfeabb;}
+v-span {
+  color: white;
+}
+
+.main-content {
+  padding-top: 20px;
+}
+
+.page-header {
+  padding-bottom: 20px;
+}
+
+.pad-top {
+  padding-top: 20px;
+}
+
+.accepted {
+  background-color: #bfeabb;
+}

commit 275c494d4b669c1ee3641d709b22fae55a04054c

Add tool "Retrieve suspended users on creation"
Also, fix CSS bug accidentally introduced in commit https://github.com/SOBotics/MiniTools/commit/6c2a1b22a77a40d2d89fa6b1b412fea19d83ae14

index.html
@@ -48,6 +48,9 @@
                 <li class="list-group-item">
                     <a href="/tools/get-body-comment">Get the body content of a comment</a>
                 </li>
+                <li class="list-group-item">
+                    <a href="/tools/suspended-user">Retrieve suspended users on creation</a>
+                </li>
             </ul>
         </div>
     </div>

styles/custom.css
@@ -1,4 +1,4 @@
-v-span {
+.nav-span {
   color: white;
 }
 

tools/suspended-user/index.html
@@ -1,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>MiniTools: Retrieve suspended users on creation</title>
+
+    <link href="/images/SoboticsLogo.png" rel="icon" >
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="/styles/custom.css" rel="stylesheet">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+        <a class="navbar-brand" href="/">MiniTools</a>
+        <div class="nav-quota">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <span class="nav-span">Quota - <span id="quota">0</span></span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container main-content" id="main">
+
+    <div class="row page-header text-center">
+        <h6 class="text-center">
+            Retrieve suspended users on creation
+        </h6>
+    </div>
+
+    <div class="row">
+        <div class="col-4 pad-top">
+            <label for="time-period" class="s-label">Time period in hours (int < 24)</label>
+        </div>
+        <div class="col-1 pad-top">
+            <input class="form-control" type="url" id="time-period" />
+        </div>
+        <br/>
+    </div>
+    <div class="row">
+    	<div class="col-4 pad-top">
+            <label for="sitename" class="s-label">API sitename parameter</label>
+        </div>
+        <div class="col-3 pad-top">
+            <input class="form-control" type="url" id="sitename" />
+        </div>
+     </div>
+     <div class="pad-top">
+     	<button class="btn btn-primary" type="submit" onclick="get_suspended_users()">Submit</button>
+     </div>
+    <br />
+    <br />
+    <div class="row">
+        <div class="col-12">
+            <div class="alert alert-danger alert-dismissible" role="alert" id="error" style="display:none">
+                <span id="error-msg"></span>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+        <div class="col-5">
+            <div id="data"></div>
+        </div>
+    </div>
+    
+</div>
+
+<br/>
+<br/>
+<br/>
+<hr/>
+
+<div class="container">
+    <footer class="footer">
+        <sub class="center-block"> Made by the <a href="https://sobotics.org/">SOBotics</a> team.
+            Source code on <a href="https://github.com/SOBotics/MiniTools">GitHub</a>.
+            Content provided by <a href="https://api.stackexchange.com/">Stack Exchange API</a>
+            under the <a href="https://creativecommons.org/licenses/by-sa/3.0/"> CC BY-SA 3.0 license</a>.
+        </sub>
+    </footer>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="/scripts/global.js"></script>
+<script src="suspended-user.js"></script>
+
+</body>
+
+</html>

tools/suspended-user/suspended-user.js
@@ -1,0 +1,48 @@
+const sitenames = ["stackoverflow","serverfault","superuser","meta","webapps","webapps.meta","gaming","gaming.meta","webmasters","webmasters.meta","cooking","cooking.meta","gamedev","gamedev.meta","photo","photo.meta","stats","stats.meta","math","math.meta","diy","diy.meta","meta.superuser","meta.serverfault","gis","gis.meta","tex","tex.meta","askubuntu","meta.askubuntu","money","money.meta","english","english.meta","stackapps","ux","ux.meta","unix","unix.meta","wordpress","wordpress.meta","cstheory","cstheory.meta","apple","apple.meta","rpg","rpg.meta","bicycles","bicycles.meta","softwareengineering","softwareengineering.meta","electronics","electronics.meta","android","android.meta","boardgames","boardgames.meta","physics","physics.meta","homebrew","homebrew.meta","security","security.meta","writing","writing.meta","video","video.meta","graphicdesign","graphicdesign.meta","dba","dba.meta","scifi","scifi.meta","codereview","codereview.meta","codegolf","codegolf.meta","quant","quant.meta","pm","pm.meta","skeptics","skeptics.meta","fitness","fitness.meta","drupal","drupal.meta","mechanics","mechanics.meta","parenting","parenting.meta","sharepoint","sharepoint.meta","music","music.meta","sqa","sqa.meta","judaism","judaism.meta","german","german.meta","japanese","japanese.meta","philosophy","philosophy.meta","gardening","gardening.meta","travel","travel.meta","crypto","crypto.meta","dsp","dsp.meta","french","french.meta","christianity","christianity.meta","bitcoin","bitcoin.meta","linguistics","linguistics.meta","hermeneutics","hermeneutics.meta","history","history.meta","bricks","bricks.meta","spanish","spanish.meta","scicomp","scicomp.meta","movies","movies.meta","chinese","chinese.meta","biology","biology.meta","poker","poker.meta","mathematica","mathematica.meta","psychology","psychology.meta","outdoors","outdoors.meta","martialarts","martialarts.meta","sports","sports.meta","academia","academia.meta","cs","cs.meta","workplace","workplace.meta","windowsphone","windowsphone.meta","chemistry","chemistry.meta","chess","chess.meta","raspberrypi","raspberrypi.meta","russian","russian.meta","islam","islam.meta","salesforce","salesforce.meta","patents","patents.meta","genealogy","genealogy.meta","robotics","robotics.meta","expressionengine","expressionengine.meta","politics","politics.meta","anime","anime.meta","magento","magento.meta","ell","ell.meta","sustainability","sustainability.meta","tridion","tridion.meta","reverseengineering","reverseengineering.meta","networkengineering","networkengineering.meta","opendata","opendata.meta","freelancing","freelancing.meta","blender","blender.meta","mathoverflow.net","meta.mathoverflow.net","space","space.meta","sound","sound.meta","astronomy","astronomy.meta","tor","tor.meta","pets","pets.meta","ham","ham.meta","italian","italian.meta","pt.stackoverflow","pt.meta.stackoverflow","aviation","aviation.meta","ebooks","ebooks.meta","alcohol","alcohol.meta","softwarerecs","softwarerecs.meta","arduino","arduino.meta","cs50","cs50.meta","expatriates","expatriates.meta","matheducators","matheducators.meta","meta.stackoverflow","earthscience","earthscience.meta","joomla","joomla.meta","datascience","datascience.meta","puzzling","puzzling.meta","craftcms","craftcms.meta","buddhism","buddhism.meta","hinduism","hinduism.meta","communitybuilding","communitybuilding.meta","worldbuilding","worldbuilding.meta","ja.stackoverflow","ja.meta.stackoverflow","emacs","emacs.meta","hsm","hsm.meta","economics","economics.meta","lifehacks","lifehacks.meta","engineering","engineering.meta","coffee","coffee.meta","vi","vi.meta","musicfans","musicfans.meta","woodworking","woodworking.meta","civicrm","civicrm.meta","medicalsciences","medicalsciences.meta","ru.stackoverflow","ru.meta.stackoverflow","rus","rus.meta","mythology","mythology.meta","law","law.meta","opensource","opensource.meta","elementaryos","elementaryos.meta","portuguese","portuguese.meta","computergraphics","computergraphics.meta","hardwarerecs","hardwarerecs.meta","es.stackoverflow","es.meta.stackoverflow","3dprinting","3dprinting.meta","ethereum","ethereum.meta","latin","latin.meta","languagelearning","languagelearning.meta","retrocomputing","retrocomputing.meta","crafts","crafts.meta","korean","korean.meta","monero","monero.meta","ai","ai.meta","esperanto","esperanto.meta","sitecore","sitecore.meta","iot","iot.meta","literature","literature.meta","vegetarianism","vegetarianism.meta","ukrainian","ukrainian.meta","devops","devops.meta","bioinformatics","bioinformatics.meta","cseducators","cseducators.meta","interpersonal","interpersonal.meta","iota","iota.meta","stellar","stellar.meta","conlang","conlang.meta","quantumcomputing","quantumcomputing.meta","eosio","eosio.meta","tezos","tezos.meta","or","or.meta","drones","drones.meta"];
+const api_url = STACKEXCHANGE_API + "/users";
+
+function get_suspended_users() {
+	$("#error").hide();
+    const time_period = parseInt($("#time-period").val());
+    const sitename = $("#sitename").val();
+    if (!time_period || !sitename)  { // time period or sitename is not defined
+    	display_error("You can't leave an input blank.");
+    	return;
+    } else if (!Number.isInteger(time_period) || time_period > 24 || time_period < 1) {
+    	display_error("Number must be an integer between 1 and 100.");
+    	return;
+	} else if (!sitenames.find(site => site == sitename)) { // sitename doesn't exist
+		display_error("Invalid sitename.");
+		return;
+	}
+
+	data = {
+        "site": sitename,
+        "key": API_KEY,
+        "filter": "!)RwcIAGFAE-f5H4jKSWcrvi4",
+       	"sort": "creation",
+       	"order": "desc",
+       	"fromdate": Math.round(new Date().getTime() / 1000) - time_period * 3600, // SE works with secs, not millisecs!
+       	"todate": Math.round(new Date().getTime() / 1000),
+       	"page": 1,
+       	"pagesize": 100
+	};
+	loop();
+}
+
+function loop() {
+    setTimeout(function () {
+		$.get(api_url, data, async function(results) {
+			$("#quota").html(results.quota_remaining);
+			for (let i = 0; i < results.items.length; i++) {
+				if (results.items[i].timed_penalty_date) $("#data").append(`<span style="white-space: pre;"><a href=${results.items[i].link}>${results.items[i].display_name}</a> was suspended on ${new Date(results.items[i].creation_date * 1000).toISOString()}.</span><br/>`);
+			}
+			if (results.backoff) {
+				console.warn("BACKOFF received on page", data.page, ", stopping for", results.backoff, "seconds");
+				await new Promise(a => setTimeout(a, 10000));
+			}
+			data.page++;
+			if (results.has_more) loop();
+		})
+    }, 2000);
+}